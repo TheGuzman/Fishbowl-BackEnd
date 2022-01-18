477363a3836551844471bb9a4b00f6fd8422dc5a
TheGuzman <guzmanmedrano.diego@gmail.com>
2022-01-18 15:46:41 +0100

    declared function async

--- src/adapters/mail.js
+++ src/adapters/mail.js
@@ -8,7 +8,7 @@ const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID,process.env.CL
 
 oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
 
-export const sendMail = (to, subject, content) => {
+export const sendMail = async (to, subject, content) => {
 
         const accessToken = await oAuth2Client.getAccessToken()
 
