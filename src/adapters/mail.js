6c72a74905fbce00eef572717975076f23115c7f
TheGuzman <guzmanmedrano.diego@gmail.com>
2023-06-20 19:29:41 +0200

    refactor(mail): remove google mail configuration and use smtp

--- src/adapters/mail.js
+++ src/adapters/mail.js
@@ -1,50 +1,43 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 // import { google } from 'googleapis'
 // import dotenv from 'dotenv';
 // dotenv.config();
 
-const MAIL_USER = process.env.USER_EMAIL
-const MAIL_PASS = process.env.USER_PASS
+const MAIL_USER = process.env.USER_EMAIL;
+const MAIL_PASS = process.env.USER_PASS;
 
 // const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID,process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
 
 // oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
 
 export const sendMail = (to, subject, content) => {
+  // const accessToken = await oAuth2Client.getAccessToken()
 
-        // const accessToken = await oAuth2Client.getAccessToken()
+  const transporter = nodemailer.createTransport({
+    host: account.smtp.host,
+    port: account.smtp.port,
+    secure: account.smtp.secure,
+    auth: {
+      // usuario y pass (ej: xxx@gmail.com)
+      user: account.user,
+      pass: account.pass,
+    },
+  });
+  const message = {
+    from: `FISHBOWL APP <${MAIL_USER}>`,
+    // 'Fishbowl APP <no-reply-fishbowlappfordiscussions@gmail.com>',
+    to,
+    subject,
+    html: content,
+    text: content,
+  };
 
-        const transporter = nodemailer.createTransport({
-            service:'gmail',
-            type:'SMTP',
-            host: 'smtp.gmail.com ',
-            port: 465,
-            secure: true,
-            auth: {
-                // type:'OAuth2',
-                user:MAIL_USER,
-                // clientId: process.env.CLIENT_ID,
-                // clientSecret: process.env.CLIENT_SECRET,
-                // refresh_token: process.env.REFRESH_TOKEN,
-                // accessToken: accessToken,
-                pass:MAIL_PASS
-            },
-        });
-        const message = {
-            from:`FISHBOWL APP <${MAIL_USER}>` ,
-            // 'Fishbowl APP <no-reply-fishbowlappfordiscussions@gmail.com>',
-            to, 
-            subject, 
-            html: content,
-            text: content 
-        };
-
-        transporter.sendMail(message, (err, info) => {
-            if (err) {
-                console.log('Error occurred. ' + err.message);
-                return
-            }
-            console.log('Message sent: %s', info.messageId);
-             console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        });
-}
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.log("Error occurred. " + err.message);
+      return;
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  });
+};
